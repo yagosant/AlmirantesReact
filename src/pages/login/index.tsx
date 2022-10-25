ee40e789c1a5e249c9114d03cce187a7df6e9634
lucasgomesv5 <lucasgv5dev@gmail.com>
2022-10-25 20:55:33 -0300

    feat/componenteMain

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,9 @@
 import Form from 'components/atoms/form'
+import Main from 'components/molecules/main/main-index'
 import React from 'react'
 
 export default function Login() {
   return (
-    <Form>
-
-    </Form>
+    <Main/>
   )
 }
